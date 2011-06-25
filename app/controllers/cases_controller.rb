4d4958a2467370ef3afe316a45b07fb9e0d716a8
Carlos Arenas <carlos.arenas@gmail.com>
2011-06-25 00:24:07 -0500

    Cases index view now lists open cases

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,5 +1,16 @@
 class CasesController < ApplicationController
+  require 'highrise'
+
   def index
+    configure_highrise
+    @open_kases = Highrise::Kase.open
   end
+
+  private
+
+    def configure_highrise
+      Highrise::Base.site = Site.first.url
+      Highrise::Base.user = Token.first.value
+    end
 
 end
