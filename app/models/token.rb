402dfaf90a89929c05d2cc87944ec0a42df78099
Carlos Arenas <carlos.arenas@gmail.com>
2011-06-24 22:44:38 -0500

    Added Token validations

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,3 +1,7 @@
 class Token < ActiveRecord::Base
+  validates :value,   :presence   => true,
+                      :uniqueness => true
+  validates :site_id, :presence   => true
+
   belongs_to :site
 end
