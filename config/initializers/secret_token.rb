7f1f6f06a14e26e562b457ab02b697d523bba1c3
Carlos Arenas <carlos.arenas@gmail.com>
2011-06-24 22:13:16 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superintendent::Application.config.secret_token = '62211b05c7056a0a0f61ab4d8a8b0d43dd6608162bc92dde27d372441759d5e77b9079706853c61455ab46a21542a5f2a0db950530c920c8268ea38042a24f06'
