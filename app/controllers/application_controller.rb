31f41fccd27bbf93af58350a5e1589f7e858256d
Carlos Arenas <carlos.arenas@gmail.com>
2011-06-25 21:32:20 -0500

    Moved Highrise configuration to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  require 'highrise'
+
+  def configure_highrise
+    Highrise::Base.site = Site.first.url
+    Highrise::Base.user = Token.first.value
+  end
+
 end
