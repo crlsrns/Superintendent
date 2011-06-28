abb7b57913d97a790289506720278bc16c403758
Carlos Arenas <carlos.arenas@gmail.com>
2011-06-27 22:07:42 -0500

    Removed reference to Token in abilities definition.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     can [:read, :update, :destroy], User, :id => user.id      # manage account created for him
-    can :manage, [User, Site, Token], :creator_id => user.id  # manage own creations
+    can :manage, [User, Site], :creator_id => user.id         # manage own creations
   end
 
 end
