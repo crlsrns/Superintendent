e53025367e3d41262254ed57f2b5d9c6a027b38d
Carlos Arenas <carlos.arenas@gmail.com>
2011-06-27 22:00:37 -0500

    Removed token scaffolding and updated site scaffolding

--- app/models/token.rb
+++
@@ -1,7 +0,0 @@
-class Token < ActiveRecord::Base
-  validates :value,   :presence   => true,
-                      :uniqueness => true
-  validates :site_id, :presence   => true
-
-  belongs_to :site
-end
