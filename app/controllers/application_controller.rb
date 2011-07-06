9f38653d66318b11c8106ec45cb8716eee61e27a
Carlos Arenas <carlos.arenas@gmail.com>
2011-07-06 17:58:08 -0500

    Application filters now all private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,28 +8,28 @@ class ApplicationController < ActionController::Base
 
   require 'highrise'
 
-  def configure_highrise
-    if current_site
-      Highrise::Base.site = current_site.url
-      Highrise::Base.user = current_site.api_token
-    else
-      redirect_to sites_url, :alert => "You must choose on which site to run reports."
-    end
-  end
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-
-  def default_url_options(options={})
-#    logger.debug "default_url_options is passed options: #{options.inspect}\n"
-    { :locale => I18n.locale }
-  end
-  
   private
+
+    def configure_highrise
+      if current_site
+        Highrise::Base.site = current_site.url
+        Highrise::Base.user = current_site.api_token
+      else
+        redirect_to sites_url, :alert => "You must choose on which site to run reports."
+      end
+    end
 
     def current_site
       session[:site_id] ||= Site.accessible_by(current_ability).first.id
       Site.find(session[:site_id])
     end
+
+    def default_url_options(options={})
+      { :locale => I18n.locale }
+    end
+
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
+
 end
