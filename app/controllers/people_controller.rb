bad707f97d52ebd52b6bb1457e608b15913240d7
Carlos Arenas <carlos.arenas@gmail.com>
2011-07-06 20:44:58 -0500

    Built out people index view

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,12 @@
 class PeopleController < ApplicationController
+  before_filter :configure_highrise
+  before_filter do
+    @current_site = current_site
+  end
+
   def index
+    @page_title = "People"
+    @people = Highrise::Person.find_all_across_pages
   end
 
 end
--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,7 @@
 module PeopleHelper
+
+  def url_to_person(person_id)
+    @current_site.url + '/people/' + person_id.to_s
+  end
+
 end
